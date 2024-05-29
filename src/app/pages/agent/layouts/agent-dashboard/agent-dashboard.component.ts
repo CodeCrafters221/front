a03201a3ef62dcc4f802ac517903bcc592d4f474
Papa Diallo <albaa.app@gmail.com>
2024-05-29 10:23:08 +0000

    added loan service and methods getAll & getByid

--- src/app/pages/agent/layouts/agent-dashboard/agent-dashboard.component.ts
+++ src/app/pages/agent/layouts/agent-dashboard/agent-dashboard.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { LoanService } from 'app/services/loan/loan.service';
+
+
+
+
+
+
 
 @Component({
   selector: 'app-agent-dashboard',
   templateUrl: './agent-dashboard.component.html',
   styleUrl: './agent-dashboard.component.scss'
 })
-export class AgentDashboardComponent {
+export class AgentDashboardComponent implements OnInit {
+
+
+  constructor (private loanService: LoanService) { }
+
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
