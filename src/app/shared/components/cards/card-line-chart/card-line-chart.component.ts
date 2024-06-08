72b45ea8755e82b02a57eaeaa0f89afdb970aaf0
bayelaye313 <bayelaye0510@gmail.com>
2024-06-08 12:31:03 +0000

    retouching client dash

--- src/app/shared/components/cards/card-line-chart/card-line-chart.component.ts
+++ src/app/shared/components/cards/card-line-chart/card-line-chart.component.ts
@@ -27,18 +27,11 @@ export class CardLineChartComponent  implements OnInit {
         ],
         datasets: [
           {
-            label: new Date().getFullYear(),
-            backgroundColor: "#4c51bf",
-            borderColor: "#4c51bf",
-            data: [65, 78, 66, 44, 56, 67, 75],
-            fill: false,
-          },
-          {
-            label: (new Date().getFullYear() - 1).toString(), // Convert to string
+            label: "retrait/depot",
             fill: false,
             backgroundColor: "#fff",
             borderColor: "#fff",
-            data: [40, 68, 86, 74, 56, 60, 87],
+            data: [1560, 2310, 1700.20, 2500, 998, 100],
           },
         ],
       },
