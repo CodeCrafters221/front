c9d957a5efe460dc944127226fa2f6b2590dec66
Papa Diallo <albaa.app@gmail.com>
2024-06-11 16:11:00 +0000

    feat: ai assistant api url updated -- api dockerized and deployed to render

--- src/environments/environment.debug.ts
+++ src/environments/environment.debug.ts
@@ -1,6 +1,7 @@
 export const environment = {
   apiUrl: "http://localhost:8000/api",
-  aiAssistantUrl: "http://localhost:3003/ai",
+  // aiAssistantUrl: "http://localhost:3003/ai",
+  aiAssistantUrl: "https://codecrafter-ai-1-0-0.onrender.com/ai",
   production: false,
   staging: true,
 }
