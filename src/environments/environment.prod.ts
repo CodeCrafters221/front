fc954e3ffc1e23a081ea35a5e71288f083252f54
Papa Diallo <albaa.app@gmail.com>
2024-06-21 23:33:28 +0000

    fix: api services from one port -- one url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
-  apiUrl: "https://codecraft-backend-v1-0.onrender.com/api",
-  aiAssistantUrl: "https://codecrafter-ai-1-0-0.onrender.com/ai",
+  // apiUrl: "https://codecraft-backend-v1-0.onrender.com/api",
+  apiUrl: "https://codecrafter-ai-1-0-0.onrender.com/api",
+  aiAssistantUrl: "https://codecrafter-ai-1-0-0.onrender.com/api/",
   production: true,
   staging: false,
 }
